d5909261c53920a246787bff22425e6886ebddda
Salih Can Kaya <salihcankaya2017@hotmail.com>
2025-02-19 02:18:59 +0300

    Refactor 'productController' to handle global products and vendor-specific listings

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,30 +4,61 @@ import { Vendor } from '../models/Vendor.js';
 import { generateSlug } from '../utils/generateSlug.js';
 
 export const createProduct = async (req, res) => {
-	const { name, description, price, stock, category } = req.body;
+	const {
+		name,
+		description,
+		price,
+		stock,
+		category,
+		vendorDescription,
+		vendorImages,
+	} = req.body;
 	const vendorId = req.user.userId;
 
 	try {
-		const product = new Product({
-			name,
-			description,
-			category,
+		const slug = generateSlug(name);
+
+		let product = await Product.findOne({ slug });
+
+		if (!product) {
+			product = new Product({
+				name,
+				slug,
+				description,
+				category,
+				isApproved: false,
+			});
+
+			await product.save();
+		}
+
+		const existingVendor = await ProductVendor.findOne({
+			product: product._id,
+			vendor: vendorId,
 		});
 
-		product.slug = generateSlug(name);
-		await product.save();
+		if (existingVendor) {
+			return res
+				.status(400)
+				.json({ message: 'This seller already listed this product.' });
+		}
 
 		const productVendor = new ProductVendor({
 			product: product._id,
 			vendor: vendorId,
 			price,
 			stock,
+			vendorDescription,
+			vendorImages,
+			isActive: product.isApproved,
 		});
 
-		productVendor.productSlug = generateSlug(name);
 		await productVendor.save();
 
-		res.status(201).json({ message: 'Product created successfully' });
+		res.status(201).json({
+			message:
+				'The product has been successfully added. If it is a new product, it is awaiting approval.',
+		});
 	} catch (error) {
 		res.status(400).json({ message: error.message });
 	}
@@ -35,12 +66,26 @@ export const createProduct = async (req, res) => {
 
 export const getProducts = async (req, res) => {
 	try {
-		const products = await Product.find();
+		const products = await Product.find({ isApproved: true });
 
 		if (!products.length) {
 			return res.status(404).json({ message: 'No products' });
 		}
-		res.json({ products });
+
+		const productDetails = await Promise.all(
+			products.map(async (product) => {
+				const defaultVendor = await ProductVendor.find({ product: product._id })
+					.sort({ price: 1 })
+					.populate('vendor', '_id storeName slug')
+					.limit(1);
+
+				return {
+					...product.toObject(),
+					defaultVendor: defaultVendor || null,
+				};
+			})
+		);
+		res.json({ products: productDetails });
 	} catch (error) {
 		res.status(400).json({ message: error.message });
 	}
@@ -51,17 +96,23 @@ export const getProductDetails = async (req, res) => {
 	const { magaza } = req.query;
 
 	try {
-		const productVendors = await ProductVendor.find({ productSlug: slug })
+		const product = await Product.findOne({ slug, isApproved: true });
+
+		if (!product) {
+			return res.status(404).json({ message: 'Product not found' });
+		}
+
+		let productVendors = await ProductVendor.find({ product: product._id })
 			.populate('vendor', '_id storeName slug')
 			.sort({ price: 1 });
 
 		if (productVendors.length === 0) {
-			return res.status(404).json({ message: 'Product not found!' });
+			return res
+				.status(404)
+				.json({ message: 'No listings found for this product!' });
 		}
 
-		let defaultProduct = await Product.findOne({
-			_id: productVendors[0].product,
-		});
+		let defaultVendor = productVendors[0];
 
 		if (magaza) {
 			const vendor = await Vendor.findOne({ slug: generateSlug(magaza) });
@@ -71,23 +122,26 @@ export const getProductDetails = async (req, res) => {
 			}
 
 			const vendorProduct = productVendors.find(
-				(pv) => pv.vendor._id.toString() === vendor._id.toString()
+				(listings) => listings.vendor._id.toString() === vendor._id.toString()
 			);
 
 			if (vendorProduct) {
-				defaultProduct = await Product.findOne({ _id: vendorProduct.product });
+				defaultVendor = vendorProduct;
+			} else {
+				return res.status(404).json({
+					message: `This product is not available at ${vendor.storeName}!`,
+				});
 			}
 		}
 
 		const otherVendors = productVendors.filter(
-			(pv) => pv.product._id.toString() !== defaultProduct._id.toString()
+			(listings) =>
+				listings.vendor._id.toString() !== defaultVendor.vendor._id.toString()
 		);
 
 		res.status(200).json({
-			defaultProduct,
-			productVendor: productVendors.filter(
-				(pv) => pv.product._id.toString() === defaultProduct._id.toString()
-			),
+			product,
+			defaultVendor,
 			otherVendors,
 		});
 	} catch (error) {
@@ -96,23 +150,34 @@ export const getProductDetails = async (req, res) => {
 };
 
 export const deleteProduct = async (req, res) => {
-	try {
-		const vendorId = req.user.userId;
-		const { slug } = req.params;
+	const vendorId = req.user.userId;
+	const { slug } = req.params;
 
-		const vendorProductRelation = await ProductVendor.findOne({
-			productSlug: slug,
+	try {
+		const product = await Product.findOne({ slug });
+
+		if (!product) {
+			res.status(404).json({ message: 'Product not found' });
+		}
+
+		const productVendor = await ProductVendor.findOne({
+			product: product._id,
 			vendor: vendorId,
 		});
 
-		if (!vendorProductRelation) {
+		if (!productVendor) {
 			return res
 				.status(403)
 				.json({ message: 'This product is not related to you.' });
 		}
 
-		await Product.findByIdAndDelete(vendorProductRelation.product._id);
-		await ProductVendor.findByIdAndDelete(vendorProductRelation._id);
+		const otherVendors = await ProductVendor.find({ product: product._id });
+
+		if (otherVendors.length === 1) {
+			await Product.deleteOne({ _id: product._id });
+		}
+
+		await ProductVendor.deleteOne({ _id: productVendor._id });
 
 		return res.status(200).json({ message: 'Product deleted successfully' });
 	} catch (error) {
@@ -124,46 +189,31 @@ export const searchProduct = async (req, res) => {
 	const { search } = req.query;
 
 	try {
-		const searchTerm = generateSlug(search);
-
-		const productVendors = await ProductVendor.find({
-			productSlug: { $regex: searchTerm },
-		})
-			.populate('vendor', '_id storeName slug')
-			.sort({ price: 1 });
-
-		if (productVendors.length === 0) {
-			return res.status(404).json({ message: 'Product not found!' });
-		}
-
-		const groupedProducts = {};
-		productVendors.forEach((pv) => {
-			const productSlug = pv.productSlug;
-			if (!groupedProducts[productSlug]) {
-				groupedProducts[productSlug] = [];
-			}
-			groupedProducts[productSlug].push(pv);
+		const products = await Product.find({
+			isApproved: true,
+			name: { $regex: search, $options: 'i' },
 		});
 
-		const result = [];
-		for (const productSlug in groupedProducts) {
-			const vendors = groupedProducts[productSlug];
-
-			const cheapestVendor = vendors.reduce((prev, current) => {
-				return prev.price < current.price ? prev : current;
-			});
-
-			let defaultProduct = await Product.findOne({
-				_id: cheapestVendor.product,
-			});
-
-			result.push({
-				defaultProduct,
-				productVendor: cheapestVendor,
-			});
+		if (!products.length) {
+			return res.status(404).json({ message: 'Product not found' });
 		}
 
-		res.status(200).json(result);
+		const productDetails = await Promise.all(
+			products.map(async (product) => {
+				const defaultVendor = await ProductVendor.find({
+					product: product._id,
+				})
+					.sort({ price: 1 })
+					.populate('vendor', '_id storeName slug')
+					.limit(1);
+
+				return {
+					...product.toObject(),
+					defaultVendor: defaultVendor || null,
+				};
+			})
+		);
+		res.status(200).json({ products: productDetails });
 	} catch (error) {
 		return res.status(500).json({ message: error.message });
 	}
