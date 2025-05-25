f9a24d8a1c718a29ffc2f2946daa94737a6f9226
Salih Can Kaya <salihcankaya2017@hotmail.com>
2025-05-26 00:47:30 +0300

    Add route to fetch unapproved products in adminRoutes alongside approveProduct endpoint

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 
 import { authorize, protectRoute } from '../middleware/authMiddleware.js';
-import { approveProduct } from '../controllers/adminController.js';
+import {
+	approveProduct,
+	getUnpapprovedProducts,
+} from '../controllers/adminController.js';
 
 const router = express.Router();
+
+router.get(
+	'/products/unapproved',
+	protectRoute,
+	authorize('admin'),
+	getUnpapprovedProducts
+);
 
 router.put(
 	'/approve-product/:slug',
