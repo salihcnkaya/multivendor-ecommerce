1105b582ac7a185c732afd0e0bcf131f6fa87aba
Salih Can Kaya <salihcankaya2017@hotmail.com>
2025-05-26 00:44:03 +0300

    refactor(refreshToken): use dynamic refPath for userId, add role field

--- backend/models/RefreshToken.js
+++ backend/models/RefreshToken.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const refreshTokenSchema = new mongoose.Schema(
 	{
-		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+		userId: { type: mongoose.Schema.Types.ObjectId, refPath: 'role' },
 		token: { type: String, required: true },
 		expiresAt: { type: Date, required: true },
+		role: { type: String, required: true },
 	},
 	{ timestamps: true }
 );
